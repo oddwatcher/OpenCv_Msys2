7b569fd0bc174c37a0decd8ee76243f2bc975f7b
oddwatcher <science-35@outlook.com>
2023-01-13 16:41:08 +0800

    update

--- 1.cpp
+++ 1.cpp
@@ -6,25 +6,44 @@
 #include <string>
 using namespace std;
 using namespace cv;
-int main(){
-    char i_path[20];
-    cin >> i_path;
-    Mat img;
-    img = imread(i_path,IMREAD_COLOR);
-    if (img.empty()){
-        cout<<"cannot load"<<endl;
-        waitKey(400);
-        return 1;
+int main()
+{
+    while (true)
+    {
+        char i_path[20];
+        cin >> i_path;
+        Mat img;
+        img = imread(i_path, IMREAD_COLOR);
+        if (img.empty())
+        {
+            cout << "cannot load" << endl;
+            waitKey(400);
+            return 1;
+        }
+        imshow("image", img);
+        waitKey(2000);
+        Mat out;
+        QRCodeDetector Qrc;
+        Qrc.detect(img, out);
+        string outstr;
+        outstr = Qrc.decode(img, out, out);
+        if (outstr.length() > 0)
+        {
+            cout << outstr << endl;
+            imshow("found QRcode:", out);
+            destroyWindow("image");
+            waitKey();
+            destroyWindow("found QRcode:");
+        }
+        else
+        {
+            cout << "cannot find QR code in given picture" << endl;
+            destroyWindow("image");
+        }
+        if (pollKey()=='q'){
+            break;
+        }
+        
     }
-    imshow("image",img);
-    waitKey();
-    Mat out;
-    QRCodeDetector Qrc;
-    Qrc.detect(img,out);
-    string outstr;
-    outstr = Qrc.decode(img,out,out);
-    cout << outstr <<endl;
-    imshow("found QRcode:",out);
-    waitKey();
     return 0;
 }
